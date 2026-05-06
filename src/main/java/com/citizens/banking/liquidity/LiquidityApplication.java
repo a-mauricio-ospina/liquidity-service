36c60cfe8ade46d88ae42c19a4f00292fb78d97d
Andres Ospina <andres.ospina@externos.spin.co>
2026-05-06 11:51:43 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/citizens/banking/liquidity/LiquidityApplication.java
@@ -0,0 +1,13 @@
+package com.citizens.banking.liquidity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citizens/banking/liquidity/LiquidityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizens.banking.liquidity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
